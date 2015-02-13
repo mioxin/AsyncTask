764893d6ccaf8e628d71906b3ab09ba650944613
MrMioxin <Mioxin@ya.ru>
2015-02-13 17:17:49 +0600

    Using AsyncTask.get

--- app/src/main/java/com/example/palchuk/asynctask/MainActivity.java
+++ app/src/main/java/com/example/palchuk/asynctask/MainActivity.java
@@ -3,17 +3,22 @@ package com.example.palchuk.asynctask;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 
 public class MainActivity extends ActionBarActivity {
     MyTask mt;
     TextView tv;
+    final String LOG_TAG = "MyLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,30 +28,80 @@ public class MainActivity extends ActionBarActivity {
         tv = (TextView) findViewById(R.id.tv);
     }
 
-    class MyTask extends AsyncTask<String, Integer, Void> {
+    class MyTask extends AsyncTask<Void, Void, Integer> {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
             tv.setText("Begin");
+            Log.d(LOG_TAG,"Begin.");
         }
 
         @Override
-        protected Void doInBackground(String... urls) {
+        protected Integer doInBackground(Void... params) {
             try {
-                int cnt = 0;
-                for (String url:urls) {
-                    downloadFile(url);
-                    publishProgress(++cnt);
-                }
-                TimeUnit.SECONDS.sleep(1);
+//                int cnt = 0;
+//                for (String url:urls) {
+//                    downloadFile(url);
+//                    publishProgress(++cnt);
+//                }
+                TimeUnit.SECONDS.sleep(5);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
-            return null;
+            return 100500;
+        }
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+            tv.setText("Downloaded " + values[0] + " files");
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            super.onPostExecute(result);
+            tv.setText("End. Result = " +result );
+            Log.d(LOG_TAG,"End. Result = " +result);
+        }
+
+        private void downloadFile(String url) throws InterruptedException {
+            TimeUnit.SECONDS.sleep(2);
         }
     }
 
+    public void onclick(View view) {
+//        mt = new MyTask();
+//        mt.execute("file_path_1", "file_path_2", "file_path_3", "file_path_4");
+        switch (view.getId()) {
+            case R.id.button:
+                mt = new MyTask();
+                mt.execute();
+                break;
+            case R.id.button2:
+                showResult();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void showResult() {
+        if (mt == null) return;
+        int result = -1;
+        try {
+            Log.d(LOG_TAG, "Try to get result");
+            result = mt.get(1, TimeUnit.SECONDS);
+            Log.d(LOG_TAG, "get returns " + result);
+            Toast.makeText(this, "get returns " + result, Toast.LENGTH_LONG).show();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            Log.d(LOG_TAG,"TIMEOUT. Result = " + result);
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -70,8 +125,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onclick(View view) {
-        mt = new MyTask();
-        mt.execute("file_path_1", "file_path_2", "file_path_3", "file_path_4");
-    }
-}
+ }
